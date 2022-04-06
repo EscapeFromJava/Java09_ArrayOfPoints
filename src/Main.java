1d2a8a8ed9a75e0ed22edb7798f823a61f41f7fe
Work <res>
2022-04-06 13:58:33 +0300

    v 1.2

--- src/Main.java
+++ src/Main.java
@@ -8,33 +8,39 @@ import java.util.stream.DoubleStream;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-
-        // С‡РёС‚Р°РµРј С„Р°Р№Р» РєР°Рє СЃРїРёСЃРѕРє С‚СЂРµСѓРіРѕР»СЊРЅРёРєРѕРІ Рё РІС‹РІРѕРґРёРј
+        // читаем файл как список треугольников и выводим
         String input = "input.txt";
         ArrayList<Triangle> triangleList = getTriangles(input);
-        System.out.println("РЎРїРёСЃРѕРє С‚СЂРµСѓРіРѕР»СЊРЅРёРєРѕРІ");
+        System.out.println("Список треугольников");
         triangleList.forEach(x -> System.out.println(x.toString()));
-        //СЃРѕСЂС‚РёСЂСѓРµРј РїРѕ РїР»РѕС‰Р°РґРё Рё РІС‹РІРѕРґРёРј
-        System.out.println("РЎРїРёСЃРѕРє РїР»РѕС‰Р°РґРµР№ С‚СЂРµСѓРіРѕР»СЊРЅРёРєРѕРІ РґРѕ СЃРѕСЂС‚РёСЂРѕРІРєРё");
+        //сортируем по площади и выводим
+        System.out.println("Список площадей треугольников до сортировки");
         triangleList.forEach(x -> System.out.println(x.getArea()));
         sortTriangleList(triangleList);
-        System.out.println("РЎРїРёСЃРѕРє РїР»РѕС‰Р°РґРµР№ С‚СЂРµСѓРіРѕР»СЊРЅРёРєРѕРІ РїРѕСЃР»Рµ СЃРѕСЂС‚РёСЂРѕРІРєРё");
+        System.out.println("Список площадей треугольников после сортировки");
         triangleList.forEach(x -> System.out.println(x.getArea()));
-        //РїРѕРёСЃРє С‚СЂРµСѓРіРѕР»СЊРЅРёРєР° СЃ РїР»РѕС‰Р°РґСЊСЋ Р±Р»РёР·РєРѕР№ Рє Р·Р°РґР°РЅРЅРѕР№
-        System.out.println("РўСЂРµСѓРіРѕР»СЊРЅРёРє СЃ РїРµСЂРёРјРµС‚СЂРѕРј, РЅР°РёР±РѕР»РµРµ Р±Р»РёР·РєРёРј Рє X");
-        System.out.println("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ \"X\":");
+        //поиск треугольника с площадью близкой к заданной
+        System.out.println("Список периметров треугольников");
+        triangleList.forEach(x -> System.out.println(x.getPerimeter()));
+        System.out.println("Треугольник с периметром, наиболее близким к X");
+        System.out.println("Введите число \"X\":");
         System.out.println(getMinPerimeterTriangle(triangleList).toString());
-        //РїРѕРёСЃРє С‚СЂРµСѓРіРѕР»СЊРЅРёРєР° СЃ РІРµСЂС€РёРЅРѕР№ Р±Р»РёР·РєРѕР№ Рє С‚РѕС‡РєРµ 0.0
-        System.out.println("РўСЂРµСѓРіРѕР»СЊРЅРёРє, РІРµСЂС€РёРЅР° РєРѕС‚РѕСЂРѕРіРѕ РЅР°РёР±РѕР»РµРµ Р±Р»РёР·РєР° Рє С‚РѕС‡РєРµ 0,0:");
+        //поиск треугольника с вершиной близкой к точке 0.0
+        System.out.println("Треугольник, вершина которого наиболее близка к точке 0,0:");
         minLengthToZero(triangleList);
     }
 
-    private static void minLengthToZero(ArrayList<Triangle> triangleList) {
-        double minLength = triangleList.stream().mapToDouble(Triangle::getMinLengthToZero).min().getAsDouble();
-        for (Triangle t : triangleList) {
-            if (t.getMinLengthToZero() == minLength)
-                System.out.println(t);
+    public static ArrayList<Triangle> getTriangles(String input) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(input));
+        ArrayList<Triangle> triangleList = new ArrayList<>();
+        while (sc.hasNext()) {
+            triangleList.add(new Triangle(new Point(sc.nextDouble(), sc.nextDouble()), new Point(sc.nextDouble(), sc.nextDouble()), new Point(sc.nextDouble(), sc.nextDouble())));
         }
+        return triangleList;
+    }
+
+    public static void sortTriangleList(ArrayList<Triangle> triangleList) {
+        triangleList.sort(Comparator.comparingDouble(Triangle::getArea));
     }
 
     public static Triangle getMinPerimeterTriangle(ArrayList<Triangle> triangleList) {
@@ -48,49 +54,41 @@ public class Main {
         return triangleWithMinPerimeter;
     }
 
-    public static void sortTriangleList(ArrayList<Triangle> triangleList) {
-        triangleList.sort(Comparator.comparingDouble(Triangle::getArea));
-    }
-
-    public static ArrayList<Triangle> getTriangles(String input) throws FileNotFoundException {
-        Scanner sc = new Scanner(new File(input));
-        ArrayList<Triangle> triangleList = new ArrayList<>();
-        while (sc.hasNext()) {
-            triangleList.add(new Triangle(sc.nextDouble(), sc.nextDouble(), sc.nextDouble(), sc.nextDouble(), sc.nextDouble(), sc.nextDouble()));
+    public static void minLengthToZero(ArrayList<Triangle> triangleList) {
+        double minLength = triangleList.stream().mapToDouble(Triangle::getMinLengthToZero).min().getAsDouble();
+        for (Triangle t : triangleList) {
+            if (t.getMinLengthToZero() == minLength)
+                System.out.println(t);
         }
-        return triangleList;
     }
 }
 
 class Triangle {
-    double x1, y1, x2, y2, x3, y3;
+    Point p1, p2, p3;
 
-    public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.x3 = x3;
-        this.y3 = y3;
+    public Triangle(Point p1, Point p2, Point p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
     }
 
     public double getMinLengthToZero() {
-        double d1 = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
-        double d2 = Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2));
-        double d3 = Math.sqrt(Math.pow(x3, 2) + Math.pow(y3, 2));
+        double d1 = Math.sqrt(Math.pow(p1.x, 2) + Math.pow(p1.y, 2));
+        double d2 = Math.sqrt(Math.pow(p2.x, 2) + Math.pow(p2.y, 2));
+        double d3 = Math.sqrt(Math.pow(p3.x, 2) + Math.pow(p3.y, 2));
         BigDecimal bg = new BigDecimal(Double.toString(DoubleStream.of(d1, d2, d3).min().getAsDouble()));
         return bg.setScale(2, RoundingMode.UP).doubleValue();
     }
 
     public double getArea() {
-        BigDecimal bg = new BigDecimal(Double.toString(0.5 * Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1))));
+        BigDecimal bg = new BigDecimal(Double.toString(0.5 * Math.abs((p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y))));
         return bg.setScale(2, RoundingMode.UP).doubleValue();
     }
 
     public double getPerimeter() {
-        double a1 = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
-        double a2 = Math.sqrt(Math.pow((x2 - x3), 2) + Math.pow((y2 - y3), 2));
-        double a3 = Math.sqrt(Math.pow((x3 - x1), 2) + Math.pow((y3 - y1), 2));
+        double a1 = Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
+        double a2 = Math.sqrt(Math.pow((p2.x - p3.x), 2) + Math.pow((p2.y - p3.y), 2));
+        double a3 = Math.sqrt(Math.pow((p3.x - p1.x), 2) + Math.pow((p3.y - p1.y), 2));
         BigDecimal bg = new BigDecimal(Double.toString(a1 + a2 + a3));
         return bg.setScale(2, RoundingMode.UP).doubleValue();
     }
@@ -98,11 +96,23 @@ class Triangle {
     @Override
     public String toString() {
         return this.getClass().getSimpleName() +
-                "{x1=" + x1 + ", y1=" + y1 + ", " +
-                "x2=" + x2 + ", y2=" + y2 + ", " +
-                "x3=" + x3 + ", y3=" + y3 + ", " +
+                "{p1=" + p1 + ", p2=" + p2 + ", p3=" + p3 +
                 "perimeter=" + getPerimeter() + ", " +
                 "area=" + getArea() + ", " +
                 "minToZero=" + getMinLengthToZero() + "}";
+    }
+}
+
+class Point {
+    double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "{x=" + x + ", y=" + y + "}";
     }
 }
